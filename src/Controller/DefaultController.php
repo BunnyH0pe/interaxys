96eb5d6b77aa33ccacba6099b5f710f463466f13
BunnyH0pe <clement.chenet@live.fr>
2021-05-07 11:20:37 +0200

    Update

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/defaultpage")
+     */
+    public function defaultPage(): Response
+    {
+        return $this->render('defaultpage.html.twig');
+    }
+}
